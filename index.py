4f55ea6475537b9003528e6594d053ab566f1643
Faith-Ndanu <faithndanumunyao@gmail.com>
2024-04-01 10:08:13 +0300

    added content

---
+++ index.py
@@ -0,0 +1,34 @@
+print("select an operator to perform:")
+print("1.ADD")
+print("2.SUBTRACT")
+print("3.MULTIPLY")
+print("4.DIVIDE")
+
+operation = input()
+
+operation =="1"
+num1 = input("Enter first number:")
+num2 = input("Enter second number:")
+print("The sum is "+str(Int(num1)+Int(num2)))
+
+else if operation==2:
+num1 = input("Enter first number:")
+num2 = input("Enter second number:")
+print("The sum is "+str(Int(num1)-Int(num2)))
+
+else if operation==3:
+num1 = input("Enter first number:")
+num2 = input("Enter second number:")
+print("The sum is "+str(Int(num1)*Int(num2)))
+
+else if operation==4:
+num1 = input("Enter first number:")
+num2 = input("Enter second number:")
+print("The sum is "+str(Int(num1)/Int(num2)))
+
+else
+print("Invalid Entry")
+
+
+
+
